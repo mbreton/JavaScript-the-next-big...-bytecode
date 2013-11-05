bfe9082e6db75004d1b4151b151d994cc86e0ca7
mbreton <mbreton@xebia.fr>
2013-11-05 18:46:04 +0100

    Refacto

--- js/sliders.js
+++ js/sliders.js
@@ -1,12 +1,23 @@
-var root = document.getElementById('impress');
-root.addEventListener('impress:stepenter',function(el){
-    if (typeof  window[el.target.id] == "function"){
-        window[el.target.id]();
-    }
+$(function(){
+    // Call ace tranformer on each ".editor" element 
+    $('.editor').each(function(idx, el){
+        var $el = $(el);
+        var editor = ace.edit(el.id);
+        editor.setFontSize(20);
+        editor.setTheme("ace/theme/monokai");
+        editor.getSession().setMode("ace/mode/"+$el.data('language'));
+    });
+
+    var root = document.getElementById('impress');
+    root.addEventListener('impress:stepenter',function(el){
+        if (typeof  window[el.target.id] == "function"){
+            window[el.target.id]();
+        }
+    });
 });
 
 var wtf = function(){
-    $('#js-terminal').terminal(function(command, term) {
+    var term = $('#js-terminal').terminal(function(command, term) {
         if (command !== '') {
             var result = window.eval(command);
             if (result != undefined) {
@@ -20,6 +31,20 @@ var wtf = function(){
         width: 810,
         prompt: 'js> '
     });
+    setTimeout(function(){
+        term.exec('{} + {}');
+        term.echo('^ is an empty string');
+    },3000);
+
+    setTimeout(function(){
+        term.exec('new Array() == false');
+        term.echo('weird ?');
+    },9000);
+
+    setTimeout(function(){
+        term.exec('typeof null');
+        term.echo('hum, hum ...');
+    },15000);
 }
 
 var autocomplete = function(){
@@ -30,25 +55,16 @@ var autocomplete = function(){
         '</iframe>');
 }
 
-bad_building = function(){
+var bad_building = function(){
     $('#bad_building').addClass('bad');
     setTimeout(function(){
         $('.fall').addClass('animated hinge');
     },1000);
 };
 
-coffeescript_lisible = function(){
-    coffee_editor.focus();
-};
+var coffeescript_lisible = function(){
 
-languages = function(){
-    setTimeout(function(){
-        $('#list_lang').addClass('go-up')
-    });
-};
-
-$(function(){
-    coffee_editor = ace.edit("coffee_editor");
+    var coffee_editor = ace.edit("coffee_editor");
     var coffee_result = ace.edit("coffee_result");
 
     coffee_editor.setTheme("ace/theme/monokai");
@@ -68,47 +84,11 @@ $(function(){
             console.log('mache po')
         }
     });
+    coffee_editor.focus();
+};
 
-    $('.editor').each(function(idx, el){
-        var $el = $(el);
-        var editor = ace.edit(el.id);
-        editor.setFontSize(20);
-        console.log(el.id);
-        editor.setTheme("ace/theme/monokai");
-        editor.getSession().setMode("ace/mode/"+$el.data('language'));
+var languages = function(){
+    setTimeout(function(){
+        $('#list_lang').addClass('go-up')
     });
-});
-
-/*var languages = function (){
-    var fill = d3.scale.category20();
-
-    d3.layout.cloud().size([1200, 600])
-        .words(languages_list.map(function(d) {
-                return {text: d, size: 10 + Math.random() * 90};
-        }))
-        .padding(5)
-        .rotate(function() { return ~~(Math.random() * 2) * 90; })
-        .font("Impact")
-        .fontSize(function(d) { return d.size; })
-        .on("end", draw)
-        .start();
-
-    function draw (words) {
-        d3.select("#cloud").append("svg")
-            .attr("width", 1200)
-            .attr("height", 600)
-            .append("g")
-            .attr("transform", "translate(150,150)")
-            .selectAll("text")
-            .data(words)
-            .enter().append("text")
-            .style("font-size", function(d) { return d.size + "px"; })
-            .style("font-family", "Impact")
-            .style("fill", function(d, i) { return fill(i); })
-            .attr("text-anchor", "middle")
-            .attr("transform", function(d) {
-                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-            })
-            .text(function(d) { return d.text; });
-    }
-}*/
+};
