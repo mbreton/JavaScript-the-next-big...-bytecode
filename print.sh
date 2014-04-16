3dd95ce5f891b05e4001e7a934bdf485762d923a
mbreton <mbreton@xebia.fr>
2014-04-16 14:53:50 +0200

    Split current-display fragment to v-section

--- print.sh
+++ print.sh
@@ -2,4 +2,5 @@
 
 rm -Rf print/*
 casperjs print-casper.js
-convert print/slide* -quality 100 print/JavaScript-the-next-big...bytecode.pdf
+convert print/slide* -quality 100 JavaScript-the-next-big...bytecode.pdf
+rm -Rf print/*
